25deca1b54039e417e9fe8109ed98cec23f7287c
root <root@NAS001-FileServer.proxmox.1>
2022-12-31 15:46:17 +0000

    v1.2

--- server_backup-espelhamento.sh
+++ server_backup-espelhamento.sh
@@ -13,28 +13,30 @@
 28/12/2022 - Criação
 29/12/2022 - Correção de bug de permissões
 '''
-
 #Global
-day=`date +%d`
-month=`date +%m-%B`
-year=`date +%Y`
-folderName=`date +%d%m%Y`
-logDate=`date +%d/%m/%Y-%H:%M:%S`
+day=`TZ='America/Sao_Paulo' date +%d`
+month=`TZ='America/Sao_Paulo' date +%m-%B`
+year=`TZ='America/Sao_Paulo' date +%Y`
+folderName=`TZ='America/Sao_Paulo' date +%d%m%Y`
+logDate=`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S`
 
-# Destination Server
+# Destination Server parameters
 server="192.168.0.218"
-
-# Destination Folder
 destinationFolder="/media/raid10tb/backup_arquivos/espelhamento/"
-
-# sourceFolder
 sourceFolder="/home/compartilhamentos/"
 
-# Log Folder
+# Log Parameters
 logFolder="/var/log/server_backup-sh"
+logFile="server_backup-mirror.log"
 
-# Log File Name
-logFileName="server_backup-mirror.log"
+# Check if Log directory Exists 
+if [ ! -d $logFolder ]; then
+    mkdir -p $logFolder
+fi 
 
+echo "-----------------------------------">>$logFolder/$logFile
+echo "$logDate - Espelhamento Iniciado">>$logFolder/$logFile
 # rsync command
-rsync -vaz --exclude-from='exclude_list.txt' --delete --recursive $sourceFolder root@$server:$destinationFolder
+rsync -vaz --exclude-from='exclude_list.txt' --delete --recursive $sourceFolder root@$server:$destinationFolder>>$logFolder/$logFile
+echo "$logDate - Espelhamento Finalizado">>$logFolder/$logFile
+echo ".">>$logFolder/$logFile
---
+++ server_backup-total.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+'''
+\/\/\/\/\/\/\/\ Backup do Servidor de Arquivos - Backup Completo /\/\/\/\/\/\/\/\
+
+\... Michael Martins - 2022 
+    \.... https://github.com/michaelrmartins
+
+- Faz backup da pasta de arquivos.
+- Faz backup do tipo "Completo".
+
+> Changelog
+28/12/2022 - Criação
+29/12/2022 - Correção de bug de permissões
+'''
+#Global
+day=`TZ='America/Sao_Paulo' date +%d`
+month=`TZ='America/Sao_Paulo' date +%m-%B`
+year=`TZ='America/Sao_Paulo' date +%Y`
+zipFileName=`TZ='America/Sao_Paulo' date +%d%m%Y`
+logDate=`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S`
+
+# Rsync Destination Server parameters
+server="192.168.0.218"
+destinationFolder="/media/raid10tb/backup_arquivos/backup_total_diario/"
+sourceFolder="/tmp/"
+
+# Log Parameters
+logFolder="/var/log/server_backup-sh"
+logFile="server_backup-full.log"
+
+# Check if Log directory Exists 
+if [ ! -d $logFolder ]; then
+    mkdir -p $logFolder
+fi 
+
+# Backup Begin!!!
+echo "-----------------------------------">>$logFolder/$logFile
+echo "$logDate - Backup Total Iniciado">>$logFolder/$logFile
+echo "$logDate - Compactando arquivos">>$logFolder/$logFile
+
+# Zip Command
+zip -r -y -9 -T -x@exclude_list.txt /tmp/$zipFileName.zip /home/compartilhamentos
+echo "$logDate - Compactação Finalizada, checando integridade">>$logFolder/$logFile
+echo "$logDate - Ok">>$logFolder/$logFile
+echo "$logDate - Enviando arquivo para o Servidor de Backup $server">>$logFolder/$logFile
+
+# Sending file to Server
+rsync --progress $sourceFolder/$zipFileName.zip root@$server:$destinationFolder>>$logFolder/$logFile
+
+# Clean and Exit...
+echo "$logDate - Transferência Finalizada">>$logFolder/$logFile
+echo "$logDate - Removendo arquivos temporários">>$logFolder/$logFile
+rm /tmp/*.zip
+echo echo "$logDate - Backup Total Finalizado">>$logFolder/$logFile
