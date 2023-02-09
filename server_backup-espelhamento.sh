8465a6d7fd8fcd812333f47cd5ae2c6ce4b9c87c
root <root@NAS001-FileServer.proxmox.1>
2023-02-09 11:05:18 -0400

    add users and shares folders to backup

--- server_backup-espelhamento.sh
+++ server_backup-espelhamento.sh
@@ -13,6 +13,7 @@
 > Changelog
 28/12/2022 - Criação
 29/12/2022 - Correção de bug de permissões
+09/02/2023 - Adicionada a função de espelhamento da pasta compartilhamentos e usuarios
 '''
 #Global
 day=`TZ='America/Sao_Paulo' date +%d`
@@ -23,22 +24,27 @@ logDate=`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S`
 
 # Destination Server parameters
 server="192.168.0.218"
-destinationFolder="/media/raid10tb/backup_arquivos/espelhamento/"
-sourceFolder="/home/compartilhamentos/"
+destinationFolderCompartilhamentos="/media/raid10tb/backup_arquivos/espelhamento/compartilhamentos/"
+destinationFolderUsuarios="/media/raid10tb/backup_arquivos/espelhamento/usuarios/"
+
+#Source Folders Parameters
+sourceFolderCompartilhamentos="/home/compartilhamentos/"
+sourceFolderUsuarios="/home/usuarios/"
 
 # Log Parameters
 logFolder="/var/log/server_backup-sh"
 logFile="server_backup-mirror.log"
 
-# Check if Log directory Exists
-if [ ! -d $logFolder ]; then
-    mkdir -p $logFolder
-fi
+# Begin !!!
 cd /usr/bin
 echo "-----------------------------------">>$logFolder/$logFile
-echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Espelhamento Iniciado">>$logFolder/$logFile
+echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Espelhamento da pasta CompartilhamentosIniciado">>$logFolder/$logFile
 
 # rsync command / Full Patch are necessary because cron can't work properly using local file path reference
-rsync -vaz -e "ssh -i /root/.ssh/id_rsa" --exclude-from='/home/server_backup-sh/exclude_list.txt' --delete --delete-excluded --recursive $sourceFolder root@$server:$destinationFolder>>$logFolder/$logFile
+# mirror "compartilhamentos" begin!
+rsync -vaz -e "ssh -i /root/.ssh/id_rsa" --exclude-from='/home/server_backup-sh/exclude_list.txt' --delete --delete-excluded --recursive $sourceFolderCompartilhamentos root@$server:$destinationFolderCompartilhamentos>>$logFolder/$logFile
+echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Espelhamento da Usuarios CompartilhamentosIniciado">>$logFolder/$logFile
+# mirror "usuarios" begin!
+rsync -vaz -e "ssh -i /root/.ssh/id_rsa" --exclude-from='/home/server_backup-sh/exclude_list.txt' --delete --delete-excluded --recursive $sourceFolderUsuarios root@$server:$destinationFolderUsuarios>>$logFolder/$logFile
 echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Espelhamento Finalizado">>$logFolder/$logFile
 echo ".">>$logFolder/$logFile
--- server_backup-total.sh
+++ server_backup-total.sh
@@ -13,17 +13,21 @@
 > Changelog
 28/12/2022 - Criação
 29/12/2022 - Correção de bug de permissões
+09/02/2023 - Adicionada a função de backup da pasta de usuários e setores simultaneamente
 '''
+
 #Global
 day=`TZ='America/Sao_Paulo' date +%d`
 month=`TZ='America/Sao_Paulo' date +%m-%B`
 year=`TZ='America/Sao_Paulo' date +%Y`
-zipFileName=`TZ='America/Sao_Paulo' date +%d%m%Y`
+zipFileNameCompartilhamentos="programas_`TZ='America/Sao_Paulo' date +%d%m%Y`"
+zipFileNameUsuarios="usuarios_`TZ='America/Sao_Paulo' date +%d%m%Y`"
 logDate=`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S`
 
 # Rsync Destination Server parameters
 server="192.168.0.218"
-destinationFolder="/media/raid10tb/backup_arquivos/backup_total_diario/"
+destinationFolderCompartilhamentos="/media/raid10tb/backup_arquivos/backup_total_diario/compartilhamentos"
+destinationFolderUsuarios="/media/raid10tb/backup_arquivos/backup_total_diario/usuarios"
 sourceFolder="/tmp/"
 
 # Log Parameters
@@ -41,13 +45,15 @@ echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Backup Total Iniciado">
 echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Compactando arquivos">>$logFolder/$logFile
 
 # Zip Command  / Full Patch are necessary because cron can't work properly using local file path reference
-zip -r -y -0 -T -x@/home/server_backup-sh/exclude_list.txt /tmp/$zipFileName.zip /home/compartilhamentos
+zip -r -y -0 -T -x@/home/server_backup-sh/exclude_list.txt /tmp/$zipFileNameCompartilhamentos.zip /home/compartilhamentos
+zip -r -y -0 -T -x@/home/server_backup-sh/exclude_list.txt /tmp/$zipFileNameUsuarios.zip /home/usuarios
 echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Compactação Finalizada, checando integridade">>$logFolder/$logFile
 echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Ok">>$logFolder/$logFile
 echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Enviando arquivo para o Servidor de Backup $server">>$logFolder/$logFile
 
 # Sending file to Server
-rsync --progress -e "ssh -i /root/.ssh/id_rsa" $sourceFolder/$zipFileName.zip root@$server:$destinationFolder>>$logFolder/$logFile
+rsync --progress -e "ssh -i /root/.ssh/id_rsa" $sourceFolder/$zipFileNameCompartilhamentos.zip root@$server:$destinationFolderCompartilhamentos>>$logFolder/$logFile
+rsync --progress -e "ssh -i /root/.ssh/id_rsa" $sourceFolder/$zipFileNameUsuarios.zip root@$server:$destinationFolderUsuarios>>$logFolder/$logFile
 
 # Clean and Exit...
 echo "`TZ='America/Sao_Paulo' date +%d/%m/%Y-%H:%M:%S` - Transferencia Finalizada">>$logFolder/$logFile
